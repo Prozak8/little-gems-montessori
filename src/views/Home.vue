7f277c9120979d69ef72e1d9ddfffb3f187bfb23
Zak <zakary.howard94@gmail.com>
2020-04-11 12:32:18 +0200

    quote section added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img class="img-fluid my-3" alt="Little Gems Logo" src="../assets/logo.png" />
+    <div class="container py-5 mb-3">
+      <div class="row">
+        <p class="col-12 col-md-4">Montessori Accredited</p>
+        <p class="col-12 col-md-4">Regional inspectors for MEA</p>
+        <p class="col-12 col-md-4">Training teachers</p>
+      </div>
+    </div>
+
+    <div class="container bg-light mb-3">
+      <blockquote class="blockquote p-5 text-left">
+        <p>"The most important period of life is not the age of university studies, but the first one, the period from birth to the age of six ... At no other age has the child greater need for an intelligent help, and any obstacle that impedes his creative work will lessen the chance he has of achieving perfection"</p>
+        <footer class="blockquote-footer">Maria Montessori</footer>
+      </blockquote>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
