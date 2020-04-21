7e0f64ecc78dbe6eb123b0fb5acaafbd86194a8e
Zak <zakary.howard94@gmail.com>
2020-04-21 13:20:47 +0200

    Staff page content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <p>Little Gems Montessori Nursery seeks to provide a loving, caring and stimulating environment in which children may learn, grow and develop under the guidance of caring teachers, other parents and their peers, following the Montessori philosophy and method.</p>
+      <p>The ethos of a Montessori Nursery is that the education is pupil-led. Children learn and progress at their own pace, developing independence and understanding of the world around them. At Little Gems Montessori Nursery we offer your child this unique opportunity of 'self-development' by virtue of our specially trained staff as two areas of the Montessori teachers' expertise lie in:</p>
+      <p>Being skilled and careful observers who notice readiness within the child and, creating a beautiful and carefully prepared environment. These are two key aspects of Montessori education.</p>
+    </div>
+
+    <div class="container">
+      <p>The aims and objectives at Little Gems Montessori Nursery are:</p>
+      <ul>
+        <li>To promote the development of happy, confident and well-balanced children.</li>
+        <li>To provide for children age 2-6 in Cyprus. As Cyprus is a multi-cultural society we aim to meet the needs of children from diverse cultures and backgrounds.</li>
+        <li>To foster and promote emotional well-being, a positive self-image, self-confidence, self-esteem and a feeling of community.</li>
+        <li>To allow children to enjoy their childhood as the unique time it is, to play, to laugh, to relate and to unfold their potentialities.</li>
+        <li>To offer a holistic approach to the child’s development.</li>
+        <li>To lay solid foundations for future schooling where children are allowed to follow their own nature, supported by healthy boundaries.</li>
+      </ul>
+    </div>
+
+    <div class="container">
+      <p>All materials offered to the children in the Montessori classroom go from simple concrete concepts to more complex abstract concepts where we recognise and build on each child's existing knowledge - easy to difficult.</p>
+
+      <div class="item">
+        <h5>Activities of Everyday living</h5>
+        <p>The Practical Life area helps the children develop key life skills. This is done through providing the children with simple 'every day' exercises which promote the development of independence, concentration and coordination through care of self and the environment.</p>
+      </div>
+      <div class="item">
+        <h5>Sensorial Education</h5>
+        <p>The Sensorial area helps the children develop and refine their senses which further help them develop their awareness of themselves and their surroundings.</p>
+        <p>The rich variety of apparatus that attracts the children encourage them to explore and continually extend their understanding and knowledge through their senses (touch, sight, smell, auditory and stereognostic sense), in addition to promoting their development of concentration, comprehension, perception, discernment and an understanding of the world.</p>
+      </div>
+      <div class="item">
+        <h5>Knowledge and Understanding of the World</h5>
+        <p>The Cultural area encourages the children to further their knowledge and understanding of the world. Here the children are provided with activities that gradually help them acquire more detailed knowledge in the areas of biology, geography, history, science, nature and conservation.</p>
+      </div>
+      <div class="item">
+        <h5>Language and Literacy</h5>
+        <p>An indirect approach is used to facilitate reading, writing, social and comprehension skills whereby the literacy area begins with plenty of nursery rhymes and rimes as well as poetry, rhythm and rhyme, story telling, drama and language games. A book corner is provided where the children can explore books and develop an interest for books.</p>
+        <p>The literacy approach is phonic based where indirect and direct roads into reading and writing are introduced. Each piece of material builds on the child's existing knowledge which can be tailored to meet the needs of each individual child.</p>
+      </div>
+      <div class="item">
+        <h5>Numeracy</h5>
+        <p>The Maths area aims to give the child the opportunity to talk, play, touch and experiment with the relevant mathematical concepts long before the formal teaching of maths is introduced. This approach provides solid foundations for future maths.</p>
+      </div>
+      <div class="item">
+        <h5>Creativity</h5>
+        <p>An art area is available to the children where different mediums of art are made available with emphasis on free expression and the development of creative skills - easels, gluing activities, constructing collages, cutting, water colours, felt tip pens, oil pastels, crayons and so forth are provided.</p>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>STAFF</h1>
+    <div class="container-fluid">
+      <img
+        src="@/assets/leadership.jpg"
+        alt="Leadership"
+        class="d-block mx-auto my-2"
+        style="width: 50rem; max-width: 90%"
+      />
+    </div>
+
+    <div class="container text-center mb-2">
+      <h2>Our Teachers</h2>
+      <p>Expert Teachers, With a Passion For Their Subjects, A Demonstrated Love of Working With Children—And Great Individuals</p>
+      <p>At Little Gems Montessori Nursery we understand that excellent, motivated teachers are critical to delivering our unique program each and every day. We hire and develop Passionate Professionals to deliver the excellent education you have come to expect from Little Gems.</p>
+      <p>We have developed a clear approach to hiring: we don’t rely merely on teaching credentials, but instead hire to our own standards. Our preschool Montessori teachers have invested in a 3 year long training course offered by a international credential and accrediting association, such as Montessori Centre International (MCI), UK, in addition to holding degrees in higher education. All of our teachers are people who have demonstrated a love of working with children, and who are great individuals themselves—that is, honest, high-minded adults who can model upstanding character, and a passion for life.</p>
+      <p>To help our teachers and administrators meet our high standards, we extend our “prepared environment” approach to our staff: in contrast to many schools, where teachers are predominantly on their own in determining what to teach and how teach it, we provide systematic training, feedback and development to each staff member. We invest in training, sometimes for a number of years, before we expect teachers to take on a full class load. For example, in our Montessori program, we keep additional trained teachers on staff who learn under the guidance of our head teachers and Heads of Schools until they are ready to take on a class of their own. Our training never stops: teachers continue to observe other classes and have other teachers observe theirs; they participate in regular development and training sessions attended by all of our academic staff.</p>
+      <p>The goal of our hiring, training and communications systems throughout is to enable each teacher to consistently offer their best to your child, so that the quality of your child’s education meets our high standards each and every day.</p>
+    </div>
+
+    <div class="container mb-2">
+      <h2>Carola Howard</h2>
+      <p>The owner and Principal, Carola Lång-Howard – MA., BA hons., Mont. Dipl. is a qualified Montessorian with plenty years of experience in the field of Early Childhood. Her interest in Montessori began 24 years ago when she became a mother. She had the opportunity to study the Montessori philosophy and method while living in Kuwait. There she began her career by initially working as an assistant teacher while studying, and as a Directress when qualified.</p>
+      <p>In 1997 Carola and her family moved to Bahrain. There she decided to start up her own Montessori Nursery. As Montessori was very much a new concept to Bahrain she dealt a great deal with media to spread the word 'Montessori'. As a result the Montessori philosophy is now well known in Bahrain.</p>
+      <p>In 1999 Carola and her family relocated to London. Here Carola furthered her career by studying to become a Montessori teacher trainer and later began to lecture at Montessori Centre International in London. She is also a Montessori examiner as well as a teaching practice tutor for MCI as Little Gems Larnaca is licensed as an MCI Centre through Crossfields. Carola is a dedicated and true believer of the great importance of the early years where foundations are laid for life. These years cannot be ignored – attention and quality in the approach to our little ones has to be recognized, followed and nurtured. First things first!</p>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <h3>Emma Howard</h3>
+          <p>Emma is the Manager and Lead Teacher of Little Gems Larnaca, which has been open since 2013. Emma started working in Montessori settings in 2009, where she spent two years working as an Assistant Teacher and MCI student. Once she received her International Teaching Diploma in Montessori Pedagogy, she spent a year in Vancouver where she worked on a more one-to-one basis, where she adapted Montessori to her setting in English and Swedish. In 2012 she took a Lead Teacher Position in Little Gems Nicosia, and in 2013 accepted the move to Larnaca and continue to work closely with Carola in the growth of our lovely Larnaca program.</p>
+          <p>Together with Carola she organises the delivery of the Montessori Centre International Apparatus Workshop that happens every year, where students from around the world come and receive their practical diplomas. As a centre for MCI, she also tutors and mentors future Early Years Educators as they embark on their Professional Placement at Little Gems Montessori.</p>
+          <p>Before Montessori she studied Media Production at the University of Lincoln, and acts as our Media Organiser across both campuses. Outside of school you will find her on her bike, on her yoga mat, or on her surf board, in between looking for new delicious things to try! With both Swedish and British heritage and having lived in at least three continents thus far, she feels she is an adaptable, curious and welcoming member of the global society.</p>
+        </div>
+        <div class="col-12 col-md-6">
+          <h3>Elena Savva</h3>
+          <p>Elena has been part of Little Gems Montessori Nursery for the past six years. After working in traditional schools, she started at Little Gems as an Assistant Teacher. As a result she decided that the Montessori Method and Philosophy was something she wanted to learn more about. She embarked on her Montessori journey in 2010, and two and a half years later received her Teaching Diploma in Montessori Pedagogy from Montessori Centre International. Additionally, she holds a University Diploma for Children 0-3 Years Old, a Bachelors in Early Childhood Development, and the Prodimodiki training from University of Nicosia, which qualifies her to work with pre-primary aged children in Cyprus, which is the equivalence of Reception year aged children in the UK. From this diploma she has developed and adapted the Montessori Literacy materials into Greek, thus supporting our children that move on to Greek schools.</p>
+          <p>She is an enthusiastic, observant and sharing person who enjoys being surrounded and getting involved with both big and little people! In her free time she enjoys doing activities with her family, listening to music, and traveling with her nearest and dearest.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+</style>
